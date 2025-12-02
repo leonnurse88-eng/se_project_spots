cdc0bdc9e681e677345b5fedbe6c097ae5ba7bf2
Nightnurse <leon.nurse88@gmail.com>
2025-12-02 10:12:17 -0500

    Stage 5 form submission

--- scripts/index.js
+++ scripts/index.js
@@ -4,13 +4,35 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+
+const editProfileNameInput = editProfileModal.querySelector(
+  "#edit-profile-name-input"
+);
+
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#edit-profile-description-input"
+);
+
 const newPostBtn = document.querySelector(".profile__new-post-btn");
 
 const newPostModal = document.querySelector("#new-post-modal");
 
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const newPostCardForm = newPostModal.querySelector(".modal__form");
+
+const newPostNameInput = newPostModal.querySelector("#profile-name-input");
+
+const newPostLinkInput = newPostModal.querySelector(".modal__label");
+
+const profileNameEl = document.querySelector(".profile__name");
+
+const profileDescriptionEl = document.querySelector(".profile__description");
+
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -25,3 +47,21 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+function handleAddCardsubmit(evt) {
+  evt.preventDefault();
+  console.log("Profile Name");
+  console.log("Image Link");
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+newPostCardForm.addEventListener("submit", handleAddCardsubmit);
